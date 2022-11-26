42a6891cacb744d30e98345838370bda628036c7
Елена <egoncharova72@yandex.ru>
2022-11-26 12:56:00 +0400

    17

---
+++ task17/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и
+//  на выходе показывает вторую цифру этого числа.
+//  Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+//int number = new Random(). Next(100, 1000);
+// Console.WriteLine( number);
+// int res = ((number/10)%10);
+// Console.WriteLine($"{number}->{res}");
+
+int SecondDigit( int num)
+{
+    int res=(num/10)%10;
+    return res ;
+}
+Console.Write("Введите трехзначное число: ");
+int number  = Math.Abs (Convert.ToInt32 (Console.ReadLine()));
+number=Math.Abs(number);
+int resalt=SecondDigit(number);
+
+Console.WriteLine($"{number}->{resalt}");
