9ea27b12a2d237f401b43f4e8a765dded8bbfa17
Елена <egoncharova72@yandex.ru>
2022-11-26 21:39:28 +0400

    23

---
+++ task23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.t
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+using static System.Console;
+
+Write("Введите целое число:- ");
+int n = Convert.ToInt32(ReadLine());
+
+void CubNun(int a)
+{
+    int count = 1;
+    while (count <= a)
+    {
+        int resalt = Convert.ToInt32(Math.Pow(count,3));
+        WriteLine($"{count,2} {resalt,4}");
+        count++;
+    }
+
+}
+if(n>0) 
+CubNun( n);
+else WriteLine("Число не является натуральным");
