89962870926c2544aa88e72569c6249a80503f1f
Елена <egoncharova72@yandex.ru>
2022-11-26 12:49:29 +0400

    13

---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+//  или сообщает, что третьей цифры нет. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 645 -> 5  78 -> третьей цифры нет 32679 -> 6
+
+Console.Clear();
+Console.Write("введите любое целое число- ");
+int num = Math.Abs (Convert.ToInt32(Console.ReadLine()));
+
+void TherdNum(int number)
+{
+    int cons = number;
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+     Console.Write($" {cons}->{number % 10}");
+}
+if (num > 99)
+    TherdNum(num);
+else Console.WriteLine("третьей цифры нет");
