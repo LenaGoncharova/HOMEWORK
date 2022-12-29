c94ae4a559a36aff02613f65c9c912078a0d22fd
Елена <egoncharova72@yandex.ru>
2022-12-29 15:36:50 +0400

    task

---
+++ task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+  Console.Clear();
+  Console.Write("Введите число M: ");
+  int m =Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+  int n =Convert.ToInt32(Console.ReadLine());
+
+  int SummNumbers( int num1, int num2)
+  {
+    if( num1>num2) return 0;
+    return num1+SummNumbers( num1+1,num2);
+  }
+
+int  resalt=  SummNumbers( m, n);
+Console.Write( resalt );
